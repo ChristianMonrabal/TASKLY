6cfddbcd3e33a54854a2b16fae4dedbbbc7d8293
Darckfer <juliocesarcarrillorocha@gmail.com>
2025-04-08 18:02:18 +0200

    añadido login con google

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+            $finduser = User::where('google_id', $user->id)->first();
+
+            if ($finduser) {
+                Auth::login($finduser);
+                return redirect()->intended('dashboard');
+            } else {
+                $newUser = User::create([
+                    'nombre' => $user->user['given_name'],
+                    'apellidos' => $user->user['family_name'],
+                    'email' => $user->email,
+                    'google_id' => $user->id,
+                    'password' => encrypt('123456dummy'),
+                    'dni' => 'null',
+                    'rol_id' => 2,
+                ]);
+
+                Auth::login($newUser);
+                return redirect()->intended('welcome');
+            }
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('welcome');
 
-Route::get('/signin', [AuthController::class, 'showLoginForm'])->name('signin.auth');
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('signin.auth');
 Route::get('/signup', [AuthController::class, 'showSignupForm'])->name('signup.auth');
 Route::post('/signup', [AuthController::class, 'register'])->name('signup.store');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
@@ -19,3 +19,20 @@ Route::post('/logout', function () {
     request()->session()->regenerateToken();
     return redirect('/signin');
 })->name('logout');
+
+// use Laravel\Socialite\Facades\Socialite;
+
+// Route::get('/auth/redirect', function () {
+//     return Socialite::driver('google')->redirect();
+// })->name('login.google');
+
+// Route::get('/google-callback', function () {
+//     $user = Socialite::driver('google')->user();
+//     dd($user);
+//     // $user->token
+// });
+
+use App\Http\Controllers\Auth\GoogleController;
+
+Route::get('/auth/redirect', [GoogleController::class, 'redirectToGoogle'])->name('login.google');
+Route::get('/google-callback', [GoogleController::class, 'handleGoogleCallback']);
