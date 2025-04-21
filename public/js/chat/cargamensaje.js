9bd0e39f863b0fc46d50e17d51c632baaee13dad
Darckfer <juliocesarcarrillorocha@gmail.com>
2025-04-21 23:59:10 +0200

    reflejar envio de mensaje

--- public/js/chat/cargamensaje.js
+++ public/js/chat/cargamensaje.js
@@ -13,8 +13,6 @@ function cargamensaje(postulacionId) {
     // donde se imprimiran los chats y la informacion del usuario
     var seccionchat = document.getElementById("seccionchat");
     seccionchat.style.display = "";
-    var mensajes = document.getElementById("mensajes");
-    var infouser = document.getElementById("infouser");
     var frmenviomensaje = document.getElementById("frmenviomensaje");
 
     // envio de formulario adjuntando la id de usuario y la del trabajo
@@ -33,7 +31,73 @@ function cargamensaje(postulacionId) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            cargachat(postulacionId)
+            let contenidouser = "";
+            infouser.innerHTML = "";
+            data.user.forEach((dato) => {
+                contenidouser += '<div class="chat-user">';
+                contenidouser += '  <div class="chat-user-avatar">';
+                contenidouser += '   <img src="' + dato.foto_perfil + '" alt="foto de ' + dato.nombre + '">';
+                contenidouser += "  </div>";
+                contenidouser += '  <div class="chat-user-info">';
+                contenidouser += "     <h3>" + dato.nombre + " " + dato.apellidos + ' <span class="chat-indicator"></span></h3>';
+                contenidouser += "     <p>" + dato.descripcion + "</p>";
+                contenidouser += '     <div class="chat-context">Proyecto: ' + dato.descripcion + "</div>";
+                contenidouser += "  </div>";
+                contenidouser += "</div>";
+                contenidouser += '<div class="chat-actions">';
+                contenidouser += '<button class="chat-action-btn" title="Llamar">';
+                contenidouser += '    <i class="fas fa-phone"></i>';
+                contenidouser += "</button>";
+                contenidouser += '<button class="chat-action-btn" title="Videollamada">';
+                contenidouser += '    <i class="fas fa-video"></i>';
+                contenidouser += "</button>";
+                contenidouser += '<button class="chat-action-btn" title="Más opciones">';
+                contenidouser += '    <i class="fas fa-ellipsis-v"></i>';
+                contenidouser += "</button>";
+                contenidouser += "</div>";
+            });
+            infouser.innerHTML = contenidouser;
+            let frmenvio = "";
+            frmenviomensaje.innerHTML = "";
+            frmenvio += '  <div class="chat-input-actions">';
+            frmenvio += '  <input type="hidden" id="trabajo_idfrmmensaje" value="' + trabajo_id + '">';
+            frmenvio += '  <input type="hidden" id="trabajador_idfrmmensaje" value="' + trabajador_id + '">';
+            frmenvio += '  <button class="chat-input-btn" title="Adjuntar archivo">';
+            frmenvio += '      <i class="fas fa-paperclip"></i>';
+            frmenvio += "  </button>";
+            frmenvio += '  <button class="chat-input-btn" title="Emoji">';
+            frmenvio += '       <i class="far fa-smile"></i>';
+            frmenvio += "  </button>";
+            frmenvio += "  </div>";
+            frmenvio += '  <textarea class="message-input" id="mensajeaenviar" placeholder="Escribe un mensaje..."></textarea>';
+            frmenvio += '  <button onclick="mandarmensaje(' + postulacionId + ')" class="send-btn">';
+            frmenvio += '         <i class="fas fa-paper-plane"></i>';
+            frmenvio += "  </button>";
+            frmenviomensaje.innerHTML = frmenvio;
+        });
+}
+
+
+function cargachat(postulacionId) {
+    // donde se imprimiran los chats y la informacion del usuario
+    var mensajes = document.getElementById("mensajes");
+
+    // envio de formulario adjuntando la id de usuario y la del trabajo
+    var form = document.getElementById("chat" + postulacionId);
+    var formData = new FormData(form);
+    var trabajador_id = formData.get("trabajador_id");
+    fetch("/cargamensajes/", {
+        method: "POST",
+        body: formData,
+    })
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error("Error en la respuesta del servidor");
+            }
+            return response.json();
+        })
+        .then(function (data) {
             let contenido = "";
             mensajes.innerHTML = "";
             data.chats.forEach((mensaje) => {
@@ -69,64 +133,18 @@ function cargamensaje(postulacionId) {
                 }
                 mensajes.innerHTML = contenido;
             });
-
-            let contenidouser = "";
-            infouser.innerHTML = "";
-            data.user.forEach((dato) => {
-                contenidouser += '<div class="chat-user">';
-                contenidouser += '  <div class="chat-user-avatar">';
-                contenidouser += '   <img src="' +dato.foto_perfil + '" alt="foto de ' + dato.nombre +'">';
-                contenidouser += "  </div>";
-                contenidouser += '  <div class="chat-user-info">';
-                contenidouser += "     <h3>" + dato.nombre + " " + dato.apellidos + ' <span class="chat-indicator"></span></h3>';
-                contenidouser += "     <p>" + dato.descripcion + "</p>";
-                contenidouser += '     <div class="chat-context">Proyecto: ' + dato.descripcion + "</div>";
-                contenidouser += "  </div>";
-                contenidouser += "</div>";
-                contenidouser += '<div class="chat-actions">';
-                contenidouser += '<button class="chat-action-btn" title="Llamar">';
-                contenidouser += '    <i class="fas fa-phone"></i>';
-                contenidouser += "</button>";
-                contenidouser += '<button class="chat-action-btn" title="Videollamada">';
-                contenidouser += '    <i class="fas fa-video"></i>';
-                contenidouser += "</button>";
-                contenidouser += '<button class="chat-action-btn" title="Más opciones">';
-                contenidouser += '    <i class="fas fa-ellipsis-v"></i>';
-                contenidouser += "</button>";
-                contenidouser += "</div>";
-            });
-            infouser.innerHTML = contenidouser;
-            console.log(trabajador_id)
-
-            let frmenvio = "";
-            frmenviomensaje.innerHTML = "";
-            frmenvio += '  <div class="chat-input-actions">';
-            frmenvio += '  <input type="hidden" id="trabajo_idfrmmensaje" value="' + trabajo_id + '">';
-            frmenvio += '  <input type="hidden" id="trabajador_idfrmmensaje" value="' + trabajador_id + '">';
-            frmenvio += '  <button class="chat-input-btn" title="Adjuntar archivo">';
-            frmenvio += '      <i class="fas fa-paperclip"></i>';
-            frmenvio += "  </button>";
-            frmenvio += '  <button class="chat-input-btn" title="Emoji">';
-            frmenvio += '       <i class="far fa-smile"></i>';
-            frmenvio += "  </button>";
-            frmenvio += "  </div>";
-            frmenvio += '  <textarea class="message-input" id="mensajeaenviar" placeholder="Escribe un mensaje..."></textarea>';
-            frmenvio += '  <button onclick="mandarmensaje()" class="send-btn">';
-            frmenvio += '         <i class="fas fa-paper-plane"></i>';
-            frmenvio += "  </button>";
-            frmenviomensaje.innerHTML = frmenvio;
         });
 }
 
-function mandarmensaje() {
-    var mensaje = document.getElementById("mensajeaenviar").value;
+function mandarmensaje(postulacionId) {
+    var mensaje = document.getElementById("mensajeaenviar");
     var trabajo = document.getElementById("trabajo_idfrmmensaje").value;
     var trabajador = document.getElementById("trabajador_idfrmmensaje").value;
     var token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
     var formData = new FormData();
     formData.append('trabajo', trabajo);
     formData.append('trabajador', trabajador);
-    formData.append('mensaje', mensaje);
+    formData.append('mensaje', mensaje.value);
     formData.append('_token', token);
 
     fetch("/enviomensaje/", {
@@ -137,9 +155,10 @@ function mandarmensaje() {
             if (!response.ok) {
                 throw new Error("Error en la respuesta del servidor");
             }
-            return response.json();
+            return response.text();
         })
         .then(function (data) {
-            console.log(data);
+            mensaje.value = "";
+            cargachat(postulacionId)
         });
 }
